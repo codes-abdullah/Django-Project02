ed2ff50128c8ecf68b3dbfeb4cb73e122cccaca3
codes-abdullah <codes.abdullah@gmail.com>
2024-07-29 04:33:10 +0300

    v2

--- app01/views.py
+++ app01/views.py
@@ -1,8 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+todos = ['foo', 'bar', 'baz']
 
-app_name='tasks'
+class NewTaskForm(forms.Form):
+    the_charfield = forms.CharField(label='my field')
+
+
 def index(request):
-    return render(request, 'app01/index.html')
+    return render(request, 'app01/index.html', {
+        'todos' : todos
+    })
+
+
+def add(request):
+    if request.method == "POST":
+        f = NewTaskForm(request.POST)
+        if f.is_valid():
+            value = f.cleaned_data['the_charfield']
+            todos.append(value)
+            return render(request, 'app01/add.html', {
+                'form': NewTaskForm(),
+                'status': value+' - added successfully'
+            })
+        else:
+            return render(request, 'app01/add.html', {
+                'form':f
+            })
+
+    return render(request, 'app01/add.html', {
+        'form': NewTaskForm()
+    });
