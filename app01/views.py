ca3b7745d147ea02294d9b6a8d20ad6372e8c087
codes-abdullah <codes.abdullah@gmail.com>
2024-07-29 04:54:26 +0300

    ??

--- app01/views.py
+++ app01/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+todos = ['foo', 'bar', 'baz']
+
 app_name='tasks'
 def index(request):
-    return render(request, 'app01/index.html')
+    return render(request, 'app01/index.html', {
+        'todos': todos
+    })
+
+
+def add(request):
+    return render(request, 'app01/add.html')
